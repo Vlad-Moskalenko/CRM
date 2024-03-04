4e3e3c0a9b4e774f88ad0478ba2f8ccc7578ebe0
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2024-03-04 08:21:51 +0200

    feat: add active-label styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import ActiveLabel from './components/active-lable';
+
 export default function Home() {
   return (
     <main>
       <h1>Home page</h1>
+      <ActiveLabel>Active</ActiveLabel>
     </main>
   );
 }
