f80d51ed448fc3ad79ce7b4edf6309b9cc7edc6a
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2024-03-06 17:01:35 +0200

    feat: add-company-button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
+import AddCompanyButton from './components/add-company-button';
 import StatusLabel, { Status } from './components/status-lable';
-import { headers } from 'next/headers';
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-xl">Home page</h1>
       <StatusLabel status={Status.Active}>Active</StatusLabel>
+      <AddCompanyButton />
     </main>
   );
 }
